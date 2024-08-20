432525bdce3357bbb7340f6a0b529c67460a7417
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-20 06:06:32 +0000

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import logo
+from higher_lower_game import HigherLower
+def main() -> None:
+
+    start_game = HigherLower()
+    start_game.higher_lower_game()
+
+if __name__ == "__main__":
+    main()
